2a7e89c36e6d354ee79391cbeabe091fe6d11d41
faiyazchowdhury <faiyaz777@hotmail.com>
2019-09-03 17:13:01 -0400

    Added "View Average" (button) in loginsuccess (view), and added viewaverage (view and activity). Calculates the average.

--- app/src/main/java/com/example/firstprogrammingassignment/loginsuccess.java
+++ app/src/main/java/com/example/firstprogrammingassignment/loginsuccess.java
@@ -24,6 +24,7 @@ public class loginsuccess extends AppCompatActivity {
         mAuth = FirebaseAuth.getInstance();
         final Intent quiz = new Intent(this, com.example.firstprogrammingassignment.quiz.class);
         final Intent views = new Intent(this, viewscore.class);
+        final Intent viewavg = new Intent(this, viewaverage.class);
         String user = mAuth.getCurrentUser().getEmail().toString();
         TextView welcome = (TextView) findViewById(R.id.welcome);
         welcome.setText("Welcome "+user);
@@ -48,7 +49,7 @@ public class loginsuccess extends AppCompatActivity {
         viewaverage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(views);
+                startActivity(viewavg);
             }
         });
     }
